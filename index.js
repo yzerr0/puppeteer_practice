43115d3e6b4a7f34dbb8d6789d8cde31f575d26b
yz <youssef.zerroug1@gmail.com>
2024-08-24 20:04:56 -0700

    fixed fetching listing info and prices

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 const puppeteer = require('puppeteer');
-const $ = require('cheerio');
+const cheerio = require('cheerio');
 const CronJob = require('cron').CronJob;
 const nodemailer = require('nodemailer');
 
 const url = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=bmw+m5&_sacat=0&_odkw=bmw&_osacat=0"
 
+async function configureBrowser() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    return page;
+}
+
+async function checkPrice(page) {
+    await page.reload();
+    let html = await page.evaluate(() => document.body.innerHTML);
+    const $ = cheerio.load(html);
+    /*
+    $('s-item').each(() => {
+        let title = $(this).find('s-item__title').text();
+        let price = $(this).find('s-item__price').text();
+        console.log(title, price);
+    });
+    */
+    let listings = $('.s-item__info').text()
+    let prices = $('.s-item__price').text()
+    console.log(listings);
+    console.log(prices);
+}
+
+async function monitor() {
+    let page = await configureBrowser();
+    await checkPrice(page);
+}
+
+monitor();
